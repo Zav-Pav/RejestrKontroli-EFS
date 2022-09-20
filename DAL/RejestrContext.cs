0d280a37a7400a9b84df71631316e4b8f7bf2b60
Paweł Zav <zawadzki.pawel@protonmail.com>
2022-09-20 12:44:56 +0200

    Dodaj pliki projektów.

---
+++ DAL/RejestrContext.cs
@@ -0,0 +1,21 @@
+﻿using RejestrKontroli.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RejestrKontroli.DAL
+{
+    public class RejestrContext: DbContext
+    {
+        public RejestrContext(): base("connectionstring")
+        {
+
+        }
+        public DbSet<RejestrModel> Rejestr { get; set; }
+
+
+
+    }
+}
---
+++ Models/RejestrModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RejestrKontroli.Models
+{
+    public class RejestrModel
+    {
+        public int ID { get; set; }
+
+        public string Działanie { get; set; }
+
+        public string Poddziałanie { get; set; }
+
+        public string NrProjektu { get; set; }
+
+        public string TytułProjektu { get; set; }
+
+        public string Beneficjent { get; set; }
+
+        public string Zespół { get; set; }
+
+        public string RodzajKontroli { get; set; }
+
+        public DateTime TerminKontroli { get; set; }
+
+        public DateTime DataZakończenia { get; set; }
+
+        public bool WizytaMonitoringowa { get; set; }
+
+        public bool Pzp { get; set; }
+
+        public bool Konkurencyjnosc { get; set; }
+
+        public bool RozeznanieRynku { get; set; }
+
+        public DateTime DataPierwszejInformacji { get; set; }
+
+        public bool ZastrzezeniaDoInf { get; set; }
+
+        public DateTime DataOstatecznejInfo { get; set; }
+
+        public DateTime DataPodpisaniaInfo { get; set; }
+
+        public DateTime DataWpisuDoSL { get; set; }
+
+        public string UchybieniaNIeprawidlowoci { get; set; }
+
+        public bool Zalecenia { get; set; }
+
+        public DateTime DataWykonaniaZalecen { get; set; }
+
+        public bool KontrolaZakonczona { get; set; }
+
+    }
+}
